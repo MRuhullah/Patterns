b5e36d2996defc088a8ddcf18b289f1ba5547416
MRuhullah <ateebqazim11@gmail.com>
2025-09-21 09:59:51 +0530

    pattern

---
+++ Solid_Square.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void print1(int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "Up to which number you want to print the pattern? ";
+    cin >> n;
+    print1(n); // will go to line 3
+
+    return 0;
+}
